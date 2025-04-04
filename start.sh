edfb4916c1591462a77830b69ebcaf620e5b553e
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-04-04 08:11:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/JishuDeveloper/Ultra-Forward-Bot JishuDeveloper/Ultra-Forward-Bot
+  git clone https://github.com/Mr-SyD-OrG/Forw Mr-SyD-OrG/Forw
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/JishuDeveloper/Ultra-Forward-Bot -b $BRANCH /Ultra-Forward-Bot
+  git clone https://github.com/Mr-SyD-OrG/Forw -b $BRANCH /main
 fi
 cd JishuDeveloper/Ultra-Forward-Bot
 pip3 install -U -r requirements.txt
