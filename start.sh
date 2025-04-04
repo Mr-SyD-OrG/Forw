679d62f9c4dfb52d8ef4fcf4e397239f02042da5
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-04-04 08:14:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ else
   echo "Cloning $BRANCH branch...."
   git clone https://github.com/Mr-SyD-OrG/Forw -b $BRANCH /main
 fi
-cd JishuDeveloper/Ultra-Forward-Bot
+cd Mr-SyD-OrG/Forw
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
