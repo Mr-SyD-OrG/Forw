aef43f9d40c7c983e5e42e2558664fdb8b88d4a7
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-04-04 14:26:26 +0530

    Create clone.py

---
+++ plugins/clone.py
@@ -0,0 +1,20 @@
+from config import Config
+from pyrogram import Client, filters, enums
+
+
+API_ID = Config.API_ID
+API_HASH = Config.API_HASH
+ADMINS = Config.OWNER_ID
+
+@Client.on_message(filters.command("clone") & filters.user(ADMINS))
+async def clone_menu(client, message):
+    if len(message.command) == 1:
+        return await message.reply_text("**__Give The ᴅᴜᴍᴩ ᴄʜᴀɴɴᴇʟ ɪᴅ__\n\nExᴀᴍᴩʟᴇ:- `/set_dump -1002042969565`**")
+    mrsyd = message.text.split(" ", 1)[1]
+    syd = Client(
+        f"{mrsyd}", API_ID, API_HASH,
+        bot_token=mrsyd,
+        plugins={"root": "Syd"}
+    )
+    await syd.start()
+    await message.reply_text("✅")
