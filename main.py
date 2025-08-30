4f1bec81c721c75eda6eb05ba7bee3a08fbac346
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-08-30 17:57:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
-# MrSyD
-# Telegram Channel @Bot_Cracker
-# Developer @syd_xyz
-
-
 
 from bot import Bot
 
 app = Bot()
 app.run()
-
-# Credited: Jishu Developer 
-
-
-
-
-
-# MrSyD
-# Telegram Channel @Bot_Cracker
-# Developer @syd_xyz
