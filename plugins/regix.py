9015d448d69aaa28f29d431c12b300b6cb1bdfef
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-09-06 06:43:46 +0530

    Update regix.py

--- plugins/regix.py
+++ plugins/regix.py
@@ -194,7 +194,7 @@ async def edit(msg, title, status, sts):
    estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
    estimated_total_time = estimated_total_time if estimated_total_time != '' else '0 s'
 
-   text = TEXT.format(i.fetched, i.total_files, i.duplicate, i.deleted, i.skip, status, percentage, estimated_total_time, progress)
+   text = TEXT.format(i.fetched, i.total_files, i.duplicate, i.deleted, i.skip, i.filtered, status, percentage, estimated_total_time, progress)
    if status in ["cancelled", "completed"]:
       button.append(
          [InlineKeyboardButton('◈ Uᴩᴅᴀᴛᴇ ◈', url='https://t.me/Bot_Cracker'),
